134323f9a404c50b29e37d35d575f780cabc7f88
muyao <z18846090865@gmail.com>
2022-06-14 11:34:37 +0800

    main 1、增加int32位二进制输出

--- src/main/java/com/data/Main.java
+++ src/main/java/com/data/Main.java
@@ -1,5 +1,20 @@
-package com.data;public class Main {
+package com.data;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        print(1024);
+    }
+
+    /**
+     * 输出int32位二进制
+     *
+     * @param num 十进制数字
+     * @author muyao
+     * @date 2022/6/14 11:28 AM
+     */
+    public static void print(int num) {
+        for (int i = 31; i > 0; i--) {
+            System.out.print((num & (1 << i)) == 0 ? "0" : "1");
+        }
     }
 }
